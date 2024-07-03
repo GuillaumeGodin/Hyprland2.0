72d2e3ad893840915305b86e0e555a84a5376b96
GuillaumeGodin <godin170@hotmail.com>
2024-07-03 14:12:15 -0400

    python + background select

---
+++ Bash/Rsync.py
@@ -0,0 +1,95 @@
+# sudo apt-get install python3-tk
+
+import os
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+
+root = tk.Tk()
+#root.geometry("600x140")
+root.resizable(height = None, width = None)
+root.resizable(0, 0)
+root.title("Rsync")
+root.config(background="black")
+
+def Widgets():
+
+    #    pane = Frame(root, bg="black")
+    pane = Frame(root)    
+    pane.pack(fill=X, expand=True)
+
+    # 1st Row Header---------------------------------------------------------------------------------
+    head_label = Label(pane, text="Rsync", bg="blue", font="SegoeUI 14", fg="white", width=55)
+    
+    head_label.grid(row=0, column=0, columnspan=4, sticky="n")
+
+    # 2nd Row origin----------------------------------------------------------------------------------
+    origin_label = Label(pane, text="From :", bg="white", font="Arial")
+    
+    origin_label.grid(row=1, column=0, sticky="enw")
+
+    root.originText = Entry(pane, textvariable=from_Path, font="Arial", width=45)
+    
+    root.originText.grid(row=1, column=1, columnspan=2, sticky="new")
+    
+    browse_B = Button(pane, text="Browse", bg="bisque", font="Arial", command=Browse_From, relief=GROOVE, pady=0)
+    
+    browse_B.grid(row=1, column=3, sticky="wne")
+
+    # 3rd Row Destination-----------------------------------------------------------------------------
+    destination_label = Label(pane, text="To :", bg="white", font="Arial")
+    
+    destination_label.grid(row=2, column=0, sticky="enw")
+
+    root.destinationText = Entry(pane, textvariable=to_Path, font="Arial")
+    
+    root.destinationText.grid(row=2, column=1, columnspan=2, sticky="new")
+    
+    browse_B = Button(pane, text="Browse", bg="bisque", font="Arial", command=Browse_To, relief=GROOVE, pady=0)
+
+    browse_B.grid(row=2, column=3, sticky="wne")
+
+    # 4th Row Button B--------------------------------------------------------------------------------
+    Download_B = Button(pane, text="Full Sync", font="Arial", bg="thistle1", command=FullRsync, relief=GROOVE, pady=0)
+    
+    Download_B.grid(row=4, column=0, columnspan=2, sticky="ew", pady=5, padx=20)
+
+    Download_B = Button(pane, text="Sync Keep Latest", font="Arial", bg="thistle1", command=RsyncKeepLatest, relief=GROOVE, pady=0)
+    
+    Download_B.grid(row=4, column=2, columnspan=2, sticky="we", pady=5, padx=20)
+
+#-----------------------------------------------------------------------------------------------------
+def Browse_From():
+    download_Directory = filedialog.askdirectory(initialdir="YOUR DIRECTORY PATH", title="From Folder")
+
+    from_Path.set(download_Directory)
+
+def Browse_To():
+    download_Directory = filedialog.askdirectory(initialdir="YOUR DIRECTORY PATH", title="To Folder")
+
+    to_Path.set(download_Directory)
+
+
+def FullRsync():
+
+    os.system(
+        'rsync -asAX --delete --exclude "*Trash-1000" "{}/" "{}/"'.format(from_Path.get(), to_Path.get()))
+        #rsync -aAX --delete --exclude '*Trash-1000' /media/g/10TB/* /media/g/10TB\ \(Backup\)/ 
+
+def RsyncKeepLatest():
+
+    os.system(
+        'rsync -asuv "{}/" "{}/"'.format(from_Path.get(), to_Path.get()))
+
+# Creating the tkinter Variables
+video_Link = StringVar()
+video_Name = StringVar()
+from_Path = StringVar()
+to_Path = StringVar()
+
+# Calling the Widgets() function
+Widgets()
+
+# Defining infinite loop to run
+# application
+root.mainloop()
---
+++ Bash/YouTube-DL.py
@@ -0,0 +1,128 @@
+#to use in terminal for video only no audio
+#Desktop\Python\yt-dlp.exe --list-formats (link)
+#Desktop\Python\yt-dlp.exe -f (format#) Desktop/(filename) (link)
+
+#to use in terminal for playlist audio only
+#Desktop\Python\yt-dlp.exe --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist (link)
+
+#Ubuntu
+#sudo apt install yt-DownloadPlaylist
+#sudo apt install tk
+#sudo apt install python3-tk
+
+#Arch
+#sudo pacman -Syu yt-dlp
+#sudo pacman -S tk
+
+import os
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+
+root = tk.Tk()
+#root.geometry("600x140")
+root.resizable(height = None, width = None)
+root.resizable(0, 0)
+root.title("YouTube-DL")
+root.config(background="black")
+
+def Widgets():
+
+#    pane = Frame(root, bg="black")
+    pane = Frame(root)    
+    pane.pack(fill=X, expand=True)
+
+#    root.grid_columnconfigure(0, weight=0)
+#    root.grid_columnconfigure(1, weight=0)
+#    root.grid_columnconfigure(2, weight=0)
+#    root.grid_columnconfigure(3, weight=0)
+
+    # 1st Row Header----------------------------------------------------------------------------------
+    head_label = Label(pane, text="YouTube Downloader", bg="red", fg="white", font="SegoeUI 14", width=55)
+#    head_label.pack(fill=X, expand=False, side=TOP)
+    head_label.grid(row=0, column=0, columnspan=3, sticky="n")
+
+    # 2nd Row Link------------------------------------------------------------------------------------
+    link_label = Label(pane, text="YouTube link :", bg="white", font="Arial")
+
+    link_label.grid(row=1, column=0, sticky="enw")
+
+    root.linkText = Entry(pane, font="Arial", textvariable=video_Link)
+
+    root.linkText.grid(row=1, column=1, sticky="wne", columnspan=2)
+
+    # 3rd Row File Name-------------------------------------------------------------------------------
+    link_name = Label(pane, text="File Name :", bg="white", font="Arial")
+
+    link_name.grid(row=2, column=0, sticky="enw")
+
+    root.linkName = Entry(pane, font="Arial", textvariable=video_Name)
+
+    root.linkName.insert(0, "%(title)s")
+
+    root.linkName.grid(row=2, column=1, sticky="wne", columnspan=2)
+
+    # 4th Row Destination-----------------------------------------------------------------------------
+    destination_label = Label(pane, text="Destination :", bg="white", font="Arial")
+
+    destination_label.grid(row=3, column=0, sticky="enw")
+
+    root.destinationText = Entry(pane, font="Arial", textvariable=download_Path)
+
+    root.destinationText.insert(0, "/home/$USER/Music/") 
+
+    root.destinationText.grid(row=3, column=1, sticky="new")
+
+    browse_B = Button(pane, text="Browse", bg="thistle1", font="Arial", command=Browse, pady=0, relief=GROOVE)
+
+    browse_B.grid(row=3, column=2, sticky="wne")
+    
+    # 5th Row Button Video----------------------------------------------------------------------------
+    Download_Video = Button(pane, text="Video", bg="thistle1", font="Arial", command=DownloadVideo, relief=GROOVE, pady=0)
+
+    Download_Video.grid(row=4, column=0, sticky="enw", padx=30, pady=10)
+    
+    # 5th Row Button Audio----------------------------------------------------------------------------
+    Download_Audio = Button(pane, text="Audio", bg="thistle1", font="Arial", command=DownloadAudio, relief=GROOVE, pady=0)
+
+    Download_Audio.grid(row=4, column=1, sticky="new", padx=60, pady=10)
+    
+    # 5th Row Button Playlist-------------------------------------------------------------------------
+    Download_Playlist = Button(pane, text="Playlist", bg="thistle1", font="Arial", command=DownloadPlaylist, relief=GROOVE, pady=0)
+
+    Download_Playlist.grid(row=4, column=2, sticky="enw", padx=30, pady=10)
+
+def Browse():
+    download_Directory = filedialog.askdirectory(
+        initialdir="YOUR DIRECTORY PATH", title="Save Video")
+
+    download_Path.set(download_Directory)
+
+
+def DownloadVideo():
+
+    os.system(
+        'yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" --merge-output-format mp4 -o "{}/{}.mp4" {}'.format(download_Path.get(), video_Name.get(), video_Link.get()))
+
+
+def DownloadAudio():
+
+    os.system(
+        'yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "{}/{}.mp3" {}'.format(download_Path.get(), video_Name.get(), video_Link.get()))
+
+def DownloadPlaylist():
+
+    os.system(
+        'yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "{}/%(title)s.%(ext)s" --yes-playlist {}'.format(download_Path.get(), video_Link.get()))
+
+# Creating the tkinter Variables
+video_Link = StringVar()
+video_Name = StringVar()
+download_Path = StringVar()
+
+# Calling the Widgets() function
+Widgets()
+
+# Defining infinite loop to run
+# application
+root.mainloop()
